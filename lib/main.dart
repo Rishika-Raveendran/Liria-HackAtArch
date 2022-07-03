04ce411a8b203c85f1f5db082b69bc15956b502c
Rishika-Raveendran <rishikarocky14@gmail.com>
2022-07-03 06:06:06 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+// ...
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const GetMaterialApp(
     home: SplashScreen(),
   ));
